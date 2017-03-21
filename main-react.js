52a6c8e0ad4fe6b70e91f823e83ef9352793a010
Marian Hello <marian.hello@gmail.com>
2017-03-21 11:09:16 +0100

    react alternative

---
+++ main-react.js
@@ -0,0 +1,44 @@
+const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup
+
+const Page = ({ children: pageNr, type }) => {
+	return (
+  	<div className={`page page--${type}`}>
+      <div className="page--footer">
+      	<a href={`#/page${pageNr}`} className="link">Go to page {pageNr}</a>
+       </div>
+    </div>
+  );
+}
+
+function renderPage(pageNr) {
+  const type = pageNr % 2 ? 'odd' : 'even';
+	ReactDOM.render(
+      <ReactCSSTransitionGroup
+        component="div"
+        transitionName={pageNr % 2 ? 'page' : 'pagereverse'}
+        transitionEnterTimeout={250}
+        transitionLeaveTimeout={250}
+      >
+      	<Page key={pageNr} type={type}>{pageNr}</Page>
+      </ReactCSSTransitionGroup>
+    ,document.getElementById('example')
+   );
+}
+
+function getNextPageNumber() {
+	const hash = window.location.hash;
+  let pageNr = 1;
+  if (!hash.indexOf('#/page')) {
+  	pageNr = Number(hash.substr('#/page'.length)) || 1;
+		pageNr = (pageNr === 1) ? 2 : 1
+  }  
+	return pageNr;	
+}
+
+function onHashChange() {
+  renderPage(getNextPageNumber());
+}
+
+renderPage(getNextPageNumber());
+window.addEventListener('hashchange', onHashChange, false);
+
